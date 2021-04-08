fe060c7dccbe691f82a62b6fd7fe256f6e630bd0
DanielCaoSilva <47014112+DanielCaoSilva@users.noreply.github.com>
2021-04-08 16:33:34 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# bot.py
 import os
 import random
 import string
@@ -28,22 +27,22 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 
-stringMat = [r"C:\Users\dcaos\PycharmProjects\DiscordBot\babies.mp3",
-             r"C:\Users\dcaos\PycharmProjects\DiscordBot\Oh_Baby_A_Triple.mp3",
-             r"C:\Users\dcaos\PycharmProjects\DiscordBot\alright.mp3",
-             r"C:\Users\dcaos\PycharmProjects\DiscordBot\breath-stinks.mp3",
-             r"C:\Users\dcaos\PycharmProjects\DiscordBot\donkey.mp3",
-             r"C:\Users\dcaos\PycharmProjects\DiscordBot\donkey.mp3"]
-
 
 
 mp3Dict= {
-    "babies": r"C:\Users\dcaos\PycharmProjects\DiscordBot\babies.mp3",
-    "3": r"C:\Users\dcaos\PycharmProjects\DiscordBot\Oh_Baby_A_Triple.mp3",
-    "alright":  r"C:\Users\dcaos\PycharmProjects\DiscordBot\alright.mp3",
-    "donkey": r"C:\Users\dcaos\PycharmProjects\DiscordBot\donkey.mp3",
-    "breath": r"C:\Users\dcaos\PycharmProjects\DiscordBot\breath-stinks.mp3",
-    "ace": r"C:\Users\dcaos\PycharmProjects\DiscordBot\aceu.mp3"
+    "babies": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\babies.mp3",
+    "3": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\Oh_Baby_A_Triple.mp3",
+    "alright":  r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\alright.mp3",
+    "donkey": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\donkey.mp3",
+    "breath": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\breath-stinks.mp3",
+    "ace": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\aceu.mp3",
+    "my man": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\my_man.wav",
+    "bitch": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\Awww_Bitch.wav",
+    "aids": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\AIDS!.wav",
+    "horn": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\mlg-airhorn.mp3",
+    "bruh": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\Bruh_Short.mp3",
+    "wasted": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\GTA V WastedBusted Sound Effect.mp3",
+    "noscope": r"C:\Users\dcaos\PycharmProjects\DiscordBot\sounds\get_noscoped.mp3"
 }
 
 #Get videos from links or from youtube search
@@ -53,17 +52,6 @@ def search(arg):
         except: info = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
         else: info = ydl.extract_info(arg, download=False)
     return info, info['formats'][0]['url']
-
-# def get_Audio_Path(arg):
-#     switcher = {
-#         1: r"C:\Users\dcaos\PycharmProjects\DiscordBot\babies.mp3",
-#         2: r"C:\Users\dcaos\PycharmProjects\DiscordBot\Oh_Baby_A_Triple.mp3",
-#     }
-#     return switcher.get(arg, "nothing")
-
-
-
-
 
 
 @client.event
@@ -128,25 +116,29 @@ async def leave(ctx):
     await ctx.voice_client.disconnect()
 
 
-# @bot.command()
-# async def stream(ctx,arg):
-#     print('trying to stream')
+@bot.command()
+async def list(ctx):
+    tempList = []
+    for key in mp3Dict:
+        tempList.append(key)
+    await ctx.send("Here are all the commands for the sound board: ")
+    await ctx.send(tempList)
 
-# @bot.command()
-# async def play(ctx):
-#     #guild = ctx.guild
-#     guild = discord.utils.get(client.guilds, name=GUILD)
-#     voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
-#     #audio_source = discord.FFmpegOpusAudio(executable="C:/path/ffmpeg.exe", source="mp3.mp3"))
-#     audio_source = discord.FFmpegPCMAudio('babies.mp3')
-#     if not voice_client.is_playing():
-#         voice_client.play(audio_source, after=None)
-
+@bot.command()
+async def hlp(ctx):
+    await ctx.send("Here are a list of the possible commands: \n"
+                   ";list\n"
+                   ";join\n"
+                   ";leave\n"
+                   ";play <Sound Board Command>\n"
+                   ";plays <YouTube Search")
 
 @bot.command()
 async def play(ctx, *, query):
     #"""Plays a file from the local filesystem"""
     #Sample Command: ;play <path_name>
+    # channel = ctx.author.voice.channel
+    # await channel.connect()
     audioPath = str(mp3Dict[str(query)])
     source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(audioPath))
     ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
@@ -161,7 +153,6 @@ async def plays(ctx, *, query):
     video, source = search(query)
     # guild = discord.utils.get(bot.guilds, name=GUILD)
     voice = dget(bot.voice_clients, guild=ctx.guild)
-
     #await join(ctx)
     #await ctx.send(f'Now playing {info['title']}.')
     voice.play(discord.FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
@@ -177,5 +168,3 @@ async def plays(ctx, *, query):
 
 #client.run(TOKEN)
 bot.run(TOKEN)
-
-
